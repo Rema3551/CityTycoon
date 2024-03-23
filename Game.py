22c48390d8b56bf2ce64633b0fe0d158e1759959
Rema <bouvysull1v4n@gmail.com>
2024-03-23 21:08:49 +0100

    Change revenu passif

--- Game.py
+++ Game.py
@@ -16,19 +16,20 @@ class Game:
         self.player = Player(player_position.x, player_position.y)
 
     def createBuildings(self, tmx_data):
-        self.poubelle1 = Building(5,0.004, "poubelle1","poubelle")
-        self.poubelle2 = Building(15,0.004, "poubelle2","poubelle")
-        self.poubelle3 = Building(20,0.004, "poubelle3","poubelle")
-        self.poubelle4 = Building(25,0.004, "poubelle4","poubelle")
-        self.poubelle5 = Building(30,0.004, "poubelle5","poubelle")
-        self.poubelle6 = Building(35,0.004, "poubelle6","poubelle")
-        self.poubelle7 = Building(40,0.004, "poubelle7","poubelle")
-        self.poubelle8 = Building(45,0.004, "poubelle8","poubelle")
-        self.poubelle9 = Building(50,0.004, "poubelle9","poubelle")
-        self.poubelle10 = Building(55,0.004, "poubelle10","poubelle")
-        self.rouge = Building(100,0.04, "rouge", "rouge")
-        self.help = Building(1000,0.001, "help","help")
-        self.orange = Building(500,0.002, "orange","orange")
+        # TODO : lvl max building
+        self.poubelle1 = Building(5,1,"poubelle1","poubelle")
+        self.poubelle2 = Building(15,1,"poubelle2","poubelle")
+        self.poubelle3 = Building(20,1,"poubelle3","poubelle")
+        self.poubelle4 = Building(25,1,"poubelle4","poubelle")
+        self.poubelle5 = Building(30,1,"poubelle5","poubelle")
+        self.poubelle6 = Building(35,1,"poubelle6","poubelle")
+        self.poubelle7 = Building(40,1,"poubelle7","poubelle")
+        self.poubelle8 = Building(45,1,"poubelle8","poubelle")
+        self.poubelle9 = Building(50,1,"poubelle9","poubelle")
+        self.poubelle10 = Building(55,1, "poubelle10","poubelle")
+        self.rouge = Building(100,10, "rouge", "rouge")
+        self.help = Building(1000,50, "help","help")
+        self.orange = Building(500,25, "orange","orange")
         self.buildings = [self.poubelle1, self.poubelle2, self.poubelle3, self.poubelle4, self.poubelle5, self.poubelle6, self.poubelle7, self.poubelle8, self.poubelle9, self.poubelle10, self.rouge, self.help, self.orange]
         
         for building in self.buildings:
--- Player.py
+++ Player.py
@@ -129,8 +129,9 @@ class Player(pygame.sprite.Sprite):
     def revenuPassif(self,game:Game):
         for batiment in self.listeBatiment:
             #print("le batiment rapporte " + str(batiment.getGain()))
-            self.addDollars(batiment.getGain())
-            #print(self.dollars)
+            self.addDollars(batiment.getGain()/60)
+
+            print(self.dollars)
 
 
     def ownBuilding(self, building)->bool:
