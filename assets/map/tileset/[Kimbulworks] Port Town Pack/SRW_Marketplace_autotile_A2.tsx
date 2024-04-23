6b6a12f3dbc21fee761c8bf86fcb7ceeea49b829
Rema <bouvysull1v4n@gmail.com>
2024-04-24 01:04:52 +0200

    avancement map3

---
+++ assets/map/tileset/[Kimbulworks] Port Town Pack/SRW_Marketplace_autotile_A2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="SRW_Marketplace_autotile_A2" tilewidth="16" tileheight="16" tilecount="1728" columns="48">
+ <image source="../Package_Marketplace_week40/Package_Marketplace/rpgmaker/MV/tilesets/SRW_Marketplace_autotile_A2.png" trans="000000" width="768" height="576"/>
+</tileset>
