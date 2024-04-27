c70d2cd77d374ef287455c77260db04282938ff4
Rema <bouvysull1v4n@gmail.com>
2024-04-28 01:38:03 +0200

    ajoutCercleMap1

--- assets/map/leviator.tsx
+++ assets/map/leviator.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="leviator" tilewidth="16" tileheight="16" tilecount="20" columns="4">
- <image source="tileset/leviator.jpg" trans="1f1f1f" width="78" height="80"/>
+<tileset version="1.10" tiledversion="1.10.2" name="leviator" tilewidth="16" tileheight="16" tilecount="550" columns="22">
+ <image source="tileset/leviator.png" trans="ffffff" width="360" height="414"/>
 </tileset>
