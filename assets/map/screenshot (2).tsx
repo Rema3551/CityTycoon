7c5143d236275ec08058fdb47c8066aa3e335c89
Rema <bouvysull1v4n@gmail.com>
2024-04-28 19:15:31 +0200

    fin house1

---
+++ assets/map/screenshot (2).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="screenshot (2)" tilewidth="16" tileheight="16" tilecount="1715" columns="49">
+ <image source="tileset/Super_Retro_World_Interior_pack_week42/every_packs_screenshots/screenshot (2).png" trans="231f21" width="792" height="561"/>
+</tileset>
