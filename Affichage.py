b5c6a28ad2b086564f458db8d29978857e765b83
Rema <bouvysull1v4n@gmail.com>
2024-05-02 17:29:32 +0200

    fin ajout house1

--- Affichage.py
+++ Affichage.py
@@ -79,7 +79,8 @@ class Affichage:
 
             house1Rect = self.tmx_data.get_object_by_name("house1")
             self.house1Rect = pygame.Rect(house1Rect.x, house1Rect.y, house1Rect.width, house1Rect.height)
-
+            boatMap3Rect = self.tmx_data.get_object_by_name("boatMap3")
+            self.boatMap3Rect = pygame.Rect(boatMap3Rect.x, boatMap3Rect.y, boatMap3Rect.width, boatMap3Rect.height)
 
         elif game.getMapStep()==MapStep.HOUSE1:
 
@@ -92,7 +93,7 @@ class Affichage:
             'left': player.get_image(2,82),
             'right': player.get_image(3,163),
             'up': player.get_image(0,246)
-        }
+            }
 
             self.tmx_data = pytmx.util_pygame.load_pygame('assets/map/house1.tmx')
             map_data = pyscroll.data.TiledMapData(self.tmx_data)
@@ -101,8 +102,8 @@ class Affichage:
             self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=42)
             self.group.add(game.player)
 
-            house1Rect = self.tmx_data.get_object_by_name("house1")
-            self.house1Rect = pygame.Rect(house1Rect.x, house1Rect.y, house1Rect.width, house1Rect.height)
+            enterHouse1 = self.tmx_data.get_object_by_name("enter_house1")
+            self.enterHouse1 = pygame.Rect(enterHouse1.x, enterHouse1.y, enterHouse1.width, enterHouse1.height)
 
         self.music = pygame.mixer.music.load('assets/music/music.mp3')
         pygame.mixer.music.play(-1)
@@ -343,10 +344,12 @@ class Affichage:
                 if player.feet.colliderect(self.house1Rect) and game.listBuildingVille3[0].getLvl() == game.listBuildingVille3[0].getLvlMax():
                     self.switchMap("house1Entry",game)
 
+                if player.feet.colliderect(self.boatMap3Rect):
+                    self.switchMap("boatMap3",game)
 
             elif game.getMapStep() == MapStep.HOUSE1:
                 if player.feet.colliderect(self.enterHouse1):
-                    self.switchMap("house1Exit",game)
+                    self.switchMap("enterHouse1",game)
 
         player.revenuPassif(game)
             
@@ -386,6 +389,7 @@ class Affichage:
             game.player.position[1] = spawnMap2.y
             if object == "car":
                 game.setMapStep(MapStep.MAP2)
+
         elif game.getMapStep() == MapStep.MAP2:
 
             if object == "car2":
@@ -411,15 +415,15 @@ class Affichage:
 
             if object == "boat":
 
-                player.sprite_sheet = pygame.image.load('assets/Images/playerMap3.png')
-                player.image = player.get_imageMap3(0,0)
-                player.image.set_colorkey([0,0,0]) #fait en sorte que ça soit transparent
-                player.rect = player.image.get_rect()
-                player.images = {
-                'down': player.get_image(0,0),
-                'left': player.get_image(2,82),
-                'right': player.get_image(3,163),
-                'up': player.get_image(0,246)
+                game.player.sprite_sheet = pygame.image.load('assets/Images/playerMap3.png')
+                game.player.image = game.player.get_imageMap3(0,0)
+                game.player.image.set_colorkey([0,0,0]) #fait en sorte que ça soit transparent
+                game.player.rect = game.player.image.get_rect()
+                game.player.images = {
+                    'down': game.player.get_imageMap3(0,0),
+                    'left': game.player.get_imageMap3(1,49),
+                    'right': game.player.get_imageMap3(1,96),
+                    'up': game.player.get_imageMap3(0,145)
                 }
 
                 self.tmx_data = pytmx.util_pygame.load_pygame('assets/map/ville3.tmx')
@@ -434,6 +438,8 @@ class Affichage:
                 
                 house1Rect = self.tmx_data.get_object_by_name("house1")
                 self.house1Rect = pygame.Rect(house1Rect.x, house1Rect.y, house1Rect.width, house1Rect.height)
+                boatMap3Rect = self.tmx_data.get_object_by_name("boatMap3")
+                self.boatMap3Rect = pygame.Rect(boatMap3Rect.x, boatMap3Rect.y, boatMap3Rect.width, boatMap3Rect.height)
 
                 spawnMap3 = self.tmx_data.get_object_by_name("spawn_map3")
                 game.player.position[0] = spawnMap3.x
@@ -444,15 +450,15 @@ class Affichage:
         elif game.getMapStep() == MapStep.MAP3:
             if object == "house1Entry":
 
-                player.sprite_sheet = pygame.image.load('assets/Images/player.png')
-                player.image = player.get_image(0,0)
-                player.image.set_colorkey([0,0,0]) #fait en sorte que ça soit transparent
-                player.rect = player.image.get_rect()
+                game.player.sprite_sheet = pygame.image.load('assets/Images/player.png')
+                game.player.image = game.player.get_image(0,0)
+                game.player.image.set_colorkey([0,0,0]) #fait en sorte que ça soit transparent
+                game.player.rect = game.player.image.get_rect()
                 player.images = {
-                    'down': player.get_image(0,0),
-                    'left': player.get_image(1,49),
-                    'right': player.get_image(1,96),
-                    'up': player.get_image(0,145)
+                'down': player.get_image(0,0),
+                'left': player.get_image(2,82),
+                'right': player.get_image(3,163),
+                'up': player.get_image(0,246)
                 }
 
                 self.tmx_data = pytmx.util_pygame.load_pygame('assets/map/house1.tmx')
@@ -474,8 +480,63 @@ class Affichage:
                 game.player.position[1] = spawnHouse1.y
 
                 game.setMapStep(MapStep.HOUSE1)
+
+            if object == "boatMap3":
+
+                self.tmx_data = pytmx.util_pygame.load_pygame('assets/map/ville2.tmx')
+                map_data = pyscroll.data.TiledMapData(self.tmx_data)
+                map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+                self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=6)
+                self.group.add(game.player)
+                self.walls=[]
+                for obj in self.tmx_data.objects:
+                    if obj.type == "collision":
+                        self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+                    
+                #rectangle de collision pour le boat
+                boat = self.tmx_data.get_object_by_name("boat")
+                self.boatRect = pygame.Rect(boat.x, boat.y, boat.width, boat.height)
+                car2 = self.tmx_data.get_object_by_name("car2")
+                self.car2Rect = pygame.Rect(car2.x, car2.y, car2.width, car2.height)
+                spawnMap2 = self.tmx_data.get_object_by_name("spawn_map2")
+                game.player.position[0] = spawnMap2.x
+                game.player.position[1] = spawnMap2.y
+                game.setMapStep(MapStep.MAP2)
+
         elif game.getMapStep() == MapStep.HOUSE1:
-            pass
+            if object == "enterHouse1":
+
+                game.player.sprite_sheet = pygame.image.load('assets/Images/playerMap3.png')
+                game.player.image = game.player.get_imageMap3(0,0)
+                game.player.image.set_colorkey([0,0,0]) #fait en sorte que ça soit transparent
+                game.player.rect = game.player.image.get_rect()
+                game.player.images = {
+                    'down': game.player.get_imageMap3(0,0),
+                    'left': game.player.get_imageMap3(1,49),
+                    'right': game.player.get_imageMap3(1,96),
+                    'up': game.player.get_imageMap3(0,145)
+                }
+
+
+                self.tmx_data = pytmx.util_pygame.load_pygame('assets/map/ville3.tmx')
+                map_data = pyscroll.data.TiledMapData(self.tmx_data)
+                map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+                self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=53)
+                self.group.add(game.player)
+                self.walls=[]
+                for obj in self.tmx_data.objects:
+                    if obj.name == "collision":
+                        self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+                
+                house1Rect = self.tmx_data.get_object_by_name("house1")
+                self.house1Rect = pygame.Rect(house1Rect.x, house1Rect.y, house1Rect.width, house1Rect.height)
+
+                spawnMap3 = self.tmx_data.get_object_by_name("spawn_map3")
+                game.player.position[0] = spawnMap3.x
+                game.player.position[1] = spawnMap3.y
+
+                game.setMapStep(MapStep.MAP3)
+                
 
             
             
