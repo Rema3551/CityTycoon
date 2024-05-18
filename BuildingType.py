21e794952a81775fc21fc703d9a6af14e5758704
Rema <bouvysull1v4n@gmail.com>
2024-05-18 12:46:03 +0200

    début rapport projet

--- BuildingType.py
+++ BuildingType.py
@@ -12,10 +12,7 @@ class BuildingType(Enum):
     HOUSE3_MAP1 = "house3_map1.png"
     HOUSE4_MAP1 = "house4_map1.png"
 
-    POUBELLE = "poubelle.png"
-    ROUGE = "rouge.png"
-    HELP = "help.png"
-    ORANGE = "orange.png"
+    
     HDV = "hdv.png"
     MAISONETTE = "maisonette.png"
     MAISON = "maison.png"
